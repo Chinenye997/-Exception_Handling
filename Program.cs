9eae9ad7a952577640f061b4988b913fbb113335
chinenye997 <edehg997@gmail.com>
2024-08-28 10:25:11 +0100

    Exception handing

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_Handling
+{
+    class Program
+    {
+        static void Main(string[] args)
+           
+        
+        {
+            try
+            {
+                Console.Write("Enter a number:");
+                int Num1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter another number:");
+                int Num2 = Convert.ToInt32(Console.ReadLine());
+                /*Console.Write("Enter another number:");
+                int Num3 = Convert.ToInt32(Console.ReadLine());*/
+
+
+                Console.WriteLine(Num1 / Num2); //+ Num3);
+            }
+            //catch(Exception e) this 2nd
+            catch(DivideByZeroException e)
+            {
+                // Console.WriteLine("Error"); // this first
+
+                // if i want to know what was worry with the error i just do
+
+                Console.WriteLine(e.Message);// With this i will know what is the error is.
+
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+
+            }
+           
+
+            Console.ReadLine();
+        }
+    }
+}
